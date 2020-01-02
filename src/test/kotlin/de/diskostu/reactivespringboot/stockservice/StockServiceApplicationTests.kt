9a70f7ba459300e4e65d9f4ff0908a6e585ed432
Denny Kluge <denny.kluge@gmail.com>
2020-01-02 22:30:49 +0100

    initial commit

---
+++ src/test/kotlin/de/diskostu/reactivespringboot/stockservice/StockServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.diskostu.reactivespringboot.stockservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
